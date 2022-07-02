7d0a41b5b1f4b2e5de23cfed93fe15e6606bcd93
Andej Nesterenko <mentatus.n@gmail.com>
2022-07-02 20:01:16 +0300

    Урок 3 закончили |

--- app/src/main/java/com/gb/m_2090_3/view/navigation/BottomBarActivity.kt
+++ app/src/main/java/com/gb/m_2090_3/view/navigation/BottomBarActivity.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import com.gb.m_2090_3.R
 import com.gb.m_2090_3.databinding.ActivityBottomBarBinding
 import com.gb.m_2090_3.databinding.ActivityViewPagerBinding
+import com.google.android.material.badge.BadgeDrawable
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
@@ -20,20 +21,32 @@ class BottomBarActivity : AppCompatActivity() {
 
 
         binding.bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.action_view_earth ->{navigateTo(EarthFragment()); true}
-                R.id.action_view_mars ->{navigateTo(MarsFragment());true}
-                R.id.action_view_system ->{navigateTo(SystemFragment());  true}
-                else ->true
+            when (it.itemId) {
+                R.id.action_view_earth -> {
+                    navigateTo(EarthFragment()); true
+                }
+                R.id.action_view_mars -> {
+                    navigateTo(MarsFragment());true
+                }
+                R.id.action_view_system -> {
+                    navigateTo(SystemFragment()); true
+                }
+                else -> true
             }
 
         }
         binding.bottomNavigationView.selectedItemId = R.id.action_view_earth
 
+        val badge = binding.bottomNavigationView.getOrCreateBadge(R.id.action_view_system)
+        //badge
+        badge.number = 1000
+        badge.maxCharacterCount = 5
+        badge.badgeGravity = BadgeDrawable.BOTTOM_START
+        //binding.bottomNavigationView.removeBadge(R.id.action_view_system)
     }
 
-    private fun navigateTo(fragment: Fragment){
-        supportFragmentManager.beginTransaction().replace(R.id.container,fragment).commit()
+    private fun navigateTo(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
     }
 
 }
