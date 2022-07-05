2cea0d913d2feec5d9034b9471a72b7e3633fb28
Andej Nesterenko <mentatus.n@gmail.com>
2022-07-05 22:13:17 +0300

    урок 4  закончили |

--- app/src/main/java/com/gb/m_2090_3/utils/MyImageView.kt
+++ app/src/main/java/com/gb/m_2090_3/utils/MyImageView.kt
@@ -3,14 +3,20 @@ package com.gb.m_2090_3.utils
 import android.content.Context
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.gb.m_2090_3.view.layouts.behaviors.ButtonBehavior
 
 class MyImageView @JvmOverloads constructor(
     context: Context,
     attributeSet: AttributeSet? = null,
     defStyle: Int = 0
-) : AppCompatImageView(context, attributeSet, defStyle) {
+) : AppCompatImageView(context, attributeSet, defStyle),CoordinatorLayout.AttachedBehavior {
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+    }
+
+    override fun getBehavior(): CoordinatorLayout.Behavior<*> {
+        return  ButtonBehavior(context) // 3 способ
     }
 }
--- app/src/main/java/com/gb/m_2090_3/view/layouts/CoordinatorFragment.kt
+++ app/src/main/java/com/gb/m_2090_3/view/layouts/CoordinatorFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.Fragment
 import com.gb.m_2090_3.databinding.FragmentCoordinatorBinding
+import com.gb.m_2090_3.view.layouts.behaviors.ButtonBehavior
 
 class CoordinatorFragment : Fragment() {
     private var _binding: FragmentCoordinatorBinding? = null
@@ -25,7 +27,7 @@ class CoordinatorFragment : Fragment() {
     var flag = true
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        (binding.fab.layoutParams as CoordinatorLayout.LayoutParams).behavior = ButtonBehavior(requireContext()) // 2 способ
     }
     companion object {
         @JvmStatic
