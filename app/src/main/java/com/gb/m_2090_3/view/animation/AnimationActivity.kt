5dfd994273ce9de64ebab2944f360c2c0faa698c
Andej Nesterenko <mentatus.n@gmail.com>
2022-07-08 21:17:52 +0300

    урок 5 | Движение по траектории

--- app/src/main/java/com/gb/m_2090_3/view/animation/AnimationActivity.kt
+++ app/src/main/java/com/gb/m_2090_3/view/animation/AnimationActivity.kt
@@ -2,9 +2,11 @@ package com.gb.m_2090_3.view.animation
 
 import android.graphics.Rect
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
@@ -20,7 +22,7 @@ class AnimationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAnimationBinding
 
-    var isFlag = false
+    var isFlag = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -28,33 +30,20 @@ class AnimationActivity : AppCompatActivity() {
         binding = ActivityAnimationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.imageView.setOnClickListener {
+        binding.button.setOnClickListener {
             isFlag = !isFlag
+            val params = it.layoutParams as FrameLayout.LayoutParams
 
-            val params = it.layoutParams as LinearLayout.LayoutParams
-
-            val transitionSet = TransitionSet()
-            val changeImageTransform = ChangeImageTransform()
             val changeBounds = ChangeBounds()
             changeBounds.duration = 2000L
-            changeImageTransform.duration = 2000L
-
-            transitionSet.ordering = TransitionSet.ORDERING_TOGETHER
-
-            transitionSet.addTransition(changeBounds)// важен порядок, обязетельно changeImageTransform после changeBounds
-            transitionSet.addTransition(changeImageTransform)
-
-
-
-            TransitionManager.beginDelayedTransition(binding.root, transitionSet)
+            changeBounds.setPathMotion(ArcMotion())
+            TransitionManager.beginDelayedTransition(binding.root,changeBounds)
             if (isFlag) {
-                params.height = LinearLayout.LayoutParams.MATCH_PARENT
-                binding.imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+                params.gravity = Gravity.TOP or Gravity.START
             } else {
-                params.height = LinearLayout.LayoutParams.WRAP_CONTENT
-                binding.imageView.scaleType = ImageView.ScaleType.CENTER_INSIDE
+                params.gravity = Gravity.BOTTOM or Gravity.END
             }
-            binding.imageView.layoutParams = params
+            binding.button.layoutParams = params
         }
     }
 
