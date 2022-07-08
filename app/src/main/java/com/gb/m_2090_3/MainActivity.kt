6225ef0330e8afaeb4069454bcb8e96073f1b9ae
Andej Nesterenko <mentatus.n@gmail.com>
2022-07-08 22:41:13 +0300

    урок 5 Факультатив | анимирование векторов

--- app/src/main/java/com/gb/m_2090_3/MainActivity.kt
+++ app/src/main/java/com/gb/m_2090_3/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         //setTheme(R.style.MyRedTheme)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
