1e772e89f2223f7b659ca0bcb2961f0d68b6c041
Andej Nesterenko <mentatus.n@gmail.com>
2022-07-08 20:46:46 +0300

    урок 5 | пример с explode

--- app/src/main/java/com/gb/m_2090_3/view/animation/AnimationActivity.kt
+++ app/src/main/java/com/gb/m_2090_3/view/animation/AnimationActivity.kt
@@ -1,17 +1,23 @@
 package com.gb.m_2090_3.view.animation
 
+import android.graphics.Rect
 import android.os.Bundle
-import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.transition.*
+import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.Explode
+import androidx.transition.Transition
+import androidx.transition.TransitionManager
+import com.gb.m_2090_3.R
 import com.gb.m_2090_3.databinding.ActivityAnimationBinding
+
 
 class AnimationActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityAnimationBinding
 
     var isFlag = false
@@ -22,21 +28,49 @@ class AnimationActivity : AppCompatActivity() {
         binding = ActivityAnimationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        isFlag = !isFlag
 
-        binding.button.setOnClickListener {
-            isFlag = !isFlag
-            val myAutoTransition = TransitionSet()
-            //myAutoTransition.ordering = TransitionSet.ORDERING_TOGETHER
-            myAutoTransition.ordering = TransitionSet.ORDERING_SEQUENTIAL
-            val fade = Slide(Gravity.END)
-            fade.duration = 1000L
-            val changeBounds = ChangeBounds()
-            changeBounds.duration  = 2000L
-            myAutoTransition.addTransition(changeBounds)
-            myAutoTransition.addTransition(fade)
-            TransitionManager.beginDelayedTransition(binding.transitionsContainer,myAutoTransition)
-            binding.text.visibility = if(isFlag) View.VISIBLE else {View.GONE}
+        binding.recyclerView.adapter = Adapter()
+    }
+
+    inner class Adapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            return MyViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.activity_animation_explode_recycle_view_item,
+                    parent,
+                    false
+                ) as View
+            )
         }
 
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            holder.itemView.setOnClickListener {
+
+            /*    val rect = Rect(
+                    it.x.toInt(),
+                    it.y.toInt(),
+                    it.x.toInt() + it.width.toInt(),
+                    it.x.toInt().toInt() + it.height
+                )*/
+                val rect = Rect()
+                it.getGlobalVisibleRect(rect)
+                val explode = Explode()
+                explode.duration = 2000L
+                explode.epicenterCallback = object : Transition.EpicenterCallback() {
+                    override fun onGetEpicenter(transition: Transition): Rect {
+                        return rect
+                    }
+                }
+                TransitionManager.beginDelayedTransition(binding.recyclerView, explode)
+                binding.recyclerView.adapter = null
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return 32
+        }
+
+        inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view)
     }
 }
