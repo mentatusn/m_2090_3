560cca04f731e12f0ae316a50eb225a9b3250c49
Andej Nesterenko <mentatus.n@gmail.com>
2022-07-19 20:57:44 +0300

    урок 8 | решение для визуализации загрузки/обработки данных

--- app/src/main/java/com/gb/m_2090_3/view/ux/SplashActivity.kt
+++ app/src/main/java/com/gb/m_2090_3/view/ux/SplashActivity.kt
@@ -1,8 +1,11 @@
 package com.gb.m_2090_3.view.ux
 
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
+import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.gb.m_2090_3.MainActivity
@@ -12,7 +15,15 @@ import com.gb.m_2090_3.R
 class SplashActivity : AppCompatActivity() { //TODO HW single-activity пытаемся сохранить
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.MyBlueTheme)
-        //setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_splash)
+
+        findViewById<ImageView>(R.id.icon).animate().rotation(7200f).setDuration(20000L).start()
+
+        //ObjectAnimator.ofFloat(findViewById<ImageView>(R.id.icon),View.ROTATION,720f).setDuration(2000L).start()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,UXActivity::class.java))
+            finish()
+        },2000L)
     }
 }
