12b9057dec4d0bf8528d9e4afc0372e3e7b5d7e1
Andej Nesterenko <mentatus.n@gmail.com>
2022-07-19 22:49:26 +0300

    урок 8 закончили | ViewBindingFragment для упрощения работы с binding

--- app/src/main/java/com/gb/m_2090_3/view/ux/TextUXFragment.kt
+++ app/src/main/java/com/gb/m_2090_3/view/ux/TextUXFragment.kt
@@ -1,34 +1,5 @@
 package com.gb.m_2090_3.view.ux
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.gb.m_2090_3.databinding.FragmentUxTextBinding
 
-class TextUXFragment : Fragment() {
-    private var _binding: FragmentUxTextBinding? = null
-    private val binding: FragmentUxTextBinding
-        get() = _binding!!
-
-    companion object{
-        fun newInstance() = TextUXFragment()
-    }
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentUxTextBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
-    }
-}
+class TextUXFragment : ViewBindingFragment<FragmentUxTextBinding>(FragmentUxTextBinding::inflate)
