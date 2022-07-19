574964e9a13f79d327c94d0f72cb3fac7d9392a8
Andej Nesterenko <mentatus.n@gmail.com>
2022-07-19 22:18:32 +0300

    урок 8 | setCustomAnimations

--- app/src/main/java/com/gb/m_2090_3/view/ux/UXActivity.kt
+++ app/src/main/java/com/gb/m_2090_3/view/ux/UXActivity.kt
@@ -35,7 +35,11 @@ class UXActivity : AppCompatActivity() {
     }
 
     private fun navigateTo(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
+        supportFragmentManager.beginTransaction().setCustomAnimations(R.anim.slide_in,
+            R.anim.fade_out,
+            R.anim.fade_in,
+            R.anim.slide_out
+        ).replace(R.id.container, fragment).commit()
     }
 
 }
